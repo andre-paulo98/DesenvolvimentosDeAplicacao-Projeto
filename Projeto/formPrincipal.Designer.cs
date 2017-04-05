282b3a8ee9cacb916ef9b8c7411e14bfc5e48100
Andre Carvalho <andre_r_carvalho@hotmail.com>
2017-04-05 14:57:28 +0100

    Revert

--- Projeto/formPrincipal.Designer.cs
+++ Projeto/formPrincipal.Designer.cs
@@ -28,15 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formPrincipal));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.labelUtilizadores = new System.Windows.Forms.Label();
-            this.btJogadores = new System.Windows.Forms.Button();
-            this.btEquipas = new System.Windows.Forms.Button();
+            this.btUtilizadores = new System.Windows.Forms.Button();
+            this.btBaralhos = new System.Windows.Forms.Button();
             this.btCartas = new System.Windows.Forms.Button();
             this.btTorneios = new System.Windows.Forms.Button();
-            this.btBaralhos = new System.Windows.Forms.Button();
-            this.btUtilizadores = new System.Windows.Forms.Button();
+            this.btEquipas = new System.Windows.Forms.Button();
+            this.btJogadores = new System.Windows.Forms.Button();
+            this.labelUtilizadores = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,33 +54,24 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
-            // labelUtilizadores
+            // btUtilizadores
             // 
-            this.labelUtilizadores.AutoSize = true;
-            this.labelUtilizadores.Image = ((System.Drawing.Image)(resources.GetObject("labelUtilizadores.Image")));
-            this.labelUtilizadores.Location = new System.Drawing.Point(64, 98);
-            this.labelUtilizadores.Name = "labelUtilizadores";
-            this.labelUtilizadores.Size = new System.Drawing.Size(0, 13);
-            this.labelUtilizadores.TabIndex = 1;
-            this.labelUtilizadores.Paint += new System.Windows.Forms.PaintEventHandler(this.gbUtilizadores_Paint);
+            this.btUtilizadores.Location = new System.Drawing.Point(64, 117);
+            this.btUtilizadores.Name = "btUtilizadores";
+            this.btUtilizadores.Size = new System.Drawing.Size(157, 24);
+            this.btUtilizadores.TabIndex = 0;
+            this.btUtilizadores.Text = "Utilizadores";
+            this.btUtilizadores.UseVisualStyleBackColor = true;
+            this.btUtilizadores.Paint += new System.Windows.Forms.PaintEventHandler(this.gbUtilizadores_Paint);
             // 
-            // btJogadores
+            // btBaralhos
             // 
-            this.btJogadores.Location = new System.Drawing.Point(533, 311);
-            this.btJogadores.Name = "btJogadores";
-            this.btJogadores.Size = new System.Drawing.Size(157, 24);
-            this.btJogadores.TabIndex = 0;
-            this.btJogadores.Text = "Jogadores";
-            this.btJogadores.UseVisualStyleBackColor = true;
-            // 
-            // btEquipas
-            // 
-            this.btEquipas.Location = new System.Drawing.Point(305, 311);
-            this.btEquipas.Name = "btEquipas";
-            this.btEquipas.Size = new System.Drawing.Size(157, 24);
-            this.btEquipas.TabIndex = 0;
-            this.btEquipas.Text = "Equipas";
-            this.btEquipas.UseVisualStyleBackColor = true;
+            this.btBaralhos.Location = new System.Drawing.Point(305, 117);
+            this.btBaralhos.Name = "btBaralhos";
+            this.btBaralhos.Size = new System.Drawing.Size(157, 24);
+            this.btBaralhos.TabIndex = 0;
+            this.btBaralhos.Text = "Baralhos";
+            this.btBaralhos.UseVisualStyleBackColor = true;
             // 
             // btCartas
             // 
@@ -101,25 +91,32 @@
             this.btTorneios.Text = "Torneios";
             this.btTorneios.UseVisualStyleBackColor = true;
             // 
-            // btBaralhos
+            // btEquipas
             // 
-            this.btBaralhos.Location = new System.Drawing.Point(305, 117);
-            this.btBaralhos.Name = "btBaralhos";
-            this.btBaralhos.Size = new System.Drawing.Size(157, 24);
-            this.btBaralhos.TabIndex = 0;
-            this.btBaralhos.Text = "Baralhos";
-            this.btBaralhos.UseVisualStyleBackColor = true;
+            this.btEquipas.Location = new System.Drawing.Point(305, 311);
+            this.btEquipas.Name = "btEquipas";
+            this.btEquipas.Size = new System.Drawing.Size(157, 24);
+            this.btEquipas.TabIndex = 0;
+            this.btEquipas.Text = "Equipas";
+            this.btEquipas.UseVisualStyleBackColor = true;
             // 
-            // btUtilizadores
+            // btJogadores
             // 
-            this.btUtilizadores.Location = new System.Drawing.Point(64, 117);
-            this.btUtilizadores.Name = "btUtilizadores";
-            this.btUtilizadores.Size = new System.Drawing.Size(157, 24);
-            this.btUtilizadores.TabIndex = 0;
-            this.btUtilizadores.Text = "Utilizadores";
-            this.btUtilizadores.UseVisualStyleBackColor = true;
-            this.btUtilizadores.Click += new System.EventHandler(this.btUtilizadores_Click);
-            this.btUtilizadores.Paint += new System.Windows.Forms.PaintEventHandler(this.gbUtilizadores_Paint);
+            this.btJogadores.Location = new System.Drawing.Point(533, 311);
+            this.btJogadores.Name = "btJogadores";
+            this.btJogadores.Size = new System.Drawing.Size(157, 24);
+            this.btJogadores.TabIndex = 0;
+            this.btJogadores.Text = "Jogadores";
+            this.btJogadores.UseVisualStyleBackColor = true;
+            // 
+            // labelUtilizadores
+            // 
+            this.labelUtilizadores.AutoSize = true;
+            this.labelUtilizadores.Location = new System.Drawing.Point(64, 98);
+            this.labelUtilizadores.Name = "labelUtilizadores";
+            this.labelUtilizadores.Size = new System.Drawing.Size(0, 13);
+            this.labelUtilizadores.TabIndex = 1;
+            this.labelUtilizadores.Paint += new System.Windows.Forms.PaintEventHandler(this.gbUtilizadores_Paint);
             // 
             // formPrincipal
             // 
