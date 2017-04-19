21c4567b02df5c7dd632abbfbe295577c61f123b
Andre Carvalho <andre_r_carvalho@hotmail.com>
2017-04-19 13:56:41 +0100

    Utilização da classe de repositorio

---
+++ Projeto/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto {
+    class UserRepository {
+        private Modelo_Container dbConteirner;
+
+        public UserRepository() {
+            dbConteirner = new Modelo_Container();
+        }
+
+        public List<User> UserList() {
+            List<User> UsersList = dbConteirner.User.ToList<User>();
+            return UsersList;
+        }
+
+        
+    }
+}
