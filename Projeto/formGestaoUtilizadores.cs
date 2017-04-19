c06a26fe6bbce0a9cba965f864ad03f80a3810b2
leonardo9717 <leonardo.henriques16@gmail.com>
2017-04-19 14:53:35 +0100

    Commit com as changes dos utilizadores- combo-box e verificações

--- Projeto/formGestaoUtilizadores.cs
+++ Projeto/formGestaoUtilizadores.cs
@@ -11,9 +11,64 @@ using System.Windows.Forms;
 namespace Projeto
 {
     public partial class formGestaoUtilizadores : Form {
+
+        private string username;
+        private string password;
+        private string nome;
+        private string email;
+
         public formGestaoUtilizadores()
         {
             InitializeComponent();
+
+            labelNome.Hide();
+            tbNome.Hide();
+        }
+
+        private void inserirUser(object sender, EventArgs e)
+        {
+            if (tbUsername.Text == "")
+            {
+                MessageBox.Show("Insira o Username!","Dados inválidos",MessageBoxButtons.OK,MessageBoxIcon.Warning); //Mensagem de erro se na falta de dados
+            }
+            else{ username = tbUsername.Text; }
+
+           password = tbPassword.Text;
+           nome = tbNome.Text;
+           email = tbNome.Text;
+
+            Modelo_Container container = new Modelo_Container(); //cria objeto do container
+            
+            Administrador admin = new Administrador();  //cria objeto admin
+            Referee arbrito = new Referee();  //cria objeto arbrito
+
+            if (cbCargo.SelectedIndex == 0) //inserir dados caso seja selecionado admin
+            {
+                admin.Username = username;
+                admin.Password = password;
+                admin.email = email;
+            }
+            else if (cbCargo.SelectedIndex == 1) // inserir dados caso seja selecionado arbrito
+            {
+                arbrito.Username = username;
+                arbrito.Password = password;
+                arbrito.Name = nome;
+            }
+
+        }
+
+        private void cbCargo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbCargo.SelectedIndex == 0) //muda o nome da label para email quando a combo-box muda para admin
+            {
+                labelNome.Text = "E-mail";
+            }
+            else if (cbCargo.SelectedIndex == 1)//muda o nome da label para email quando a combo-box muda para arbrito
+            {
+                labelNome.Text = "Nome";
+            }
+            labelNome.Show();
+            tbNome.Show();
         }
     }
 }
